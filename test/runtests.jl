498c38b17c58630f6e16791332ecc74ce85603ee
Ritchie Lee <ritchie.lee@gmail.com>
2018-07-27 15:17:06 -0700

    fix REQUIREs, update README, enable travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
-using GaussianProcesses2, NBInclude
+using GaussianProcesses2
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
 else
     using Test
 end
 
-
+using NBInclude
 @nbinclude("../notebooks/Regression1D.ipynb")
